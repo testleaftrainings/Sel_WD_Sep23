6c234b9428c0c89d56c04274f60557f3fb97c4c7
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2023-09-20 15:53:00 +0530

    Add files via upload

---
+++ week1/day1/FirstCoding.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class FirstCoding {
+
+	//main method is the entry point of your execution 
+	
+	public static void main(String[] args) {
+		
+		//Type  syso and then press ctrl & space together
+		/*
+		 * Type main and then press ctrl & space together
+		 * 
+		 * To run the code, right click -> Run As -> 1 Java Application
+		 * Shortcut key to execute the program is ctrl+F11
+		 * 
+		 */
+		
+		System.out.println("Hello TestLeaf");
+		System.out.println("My name is Ranjini");
+		
+	}
+
+}
